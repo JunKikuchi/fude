a3a0945fcbc289ae491b87946ab0ee7fdb599915
Jun Kikuchi <kikuchi@bonnou.com>
2012-12-15 23:17:39 +0900

    first commit.

---
+++ fude.py
@@ -0,0 +1,34 @@
+from xml.sax.saxutils import escape
+
+def html(data):
+    if isinstance(data, list):
+        if data == []:
+            return ''
+        if isinstance(data[0], list):
+            return ''.join([html(a) for a in data])
+
+        xs_class = data.pop(0).split('.')
+        xs_id = xs_class.pop(0).split('#')
+        tag = xs_id.pop(0)
+
+        _attrs = {}
+        if xs_id <> []:
+            _attrs['id'] = xs_id.pop(0)
+        if xs_class <> []:
+            _attrs['class'] = ' '.join(xs_class)
+        if data <> [] and isinstance(data[0], dict):
+            _attrs.update(data.pop(0))
+
+        attrs = ''
+        if _attrs <> {}:
+            attrs = ' ' + ' '.join([
+                '%s="%s"' % (escape(k), escape(_attrs[k]))
+                    for k in sorted(_attrs.keys())])
+
+        if data <> []:
+            container = [html(a) for a in data]
+            return '<%s%s>%s</%s>' % (tag, attrs, ''.join(container), tag)
+        else:
+            return '<%s%s />' % (tag, attrs)
+    else:
+        return data
---
+++ tests/test_fude.py
@@ -0,0 +1,73 @@
+import unittest
+from fude import html
+
+class HTMLTextCase(unittest.TestCase):
+    def test_escape_attrs(self):
+        data = ['tag#<>.<>']
+        expt = '<tag class="&lt;&gt;" id="&lt;&gt;" />'
+        self.assertEqual(html(data), expt)
+
+    def test_tag(self):
+        data = []
+        expt = ''
+        self.assertEqual(html(data), expt)
+
+        data = ['tag']
+        expt = '<tag />'
+        self.assertEqual(html(data), expt)
+
+        data = ['tag#id']
+        expt = '<tag id="id" />'
+        self.assertEqual(html(data), expt)
+
+        data = ['tag.class']
+        expt = '<tag class="class" />'
+        self.assertEqual(html(data), expt)
+
+        data = ['tag.class.a']
+        expt = '<tag class="class a" />'
+        self.assertEqual(html(data), expt)
+
+        data = ['tag#id.class']
+        expt = '<tag class="class" id="id" />'
+        self.assertEqual(html(data), expt)
+
+        data = ['tag#id.class.a']
+        expt = '<tag class="class a" id="id" />'
+        self.assertEqual(html(data), expt)
+
+        data = ['tag', {'data': 'aaa'}]
+        expt = '<tag data="aaa" />'
+        self.assertEqual(html(data), expt)
+
+        data = ['tag', {'data': 'aaa', 'id': 'id', 'class': 'class'}]
+        expt = '<tag class="class" data="aaa" id="id" />'
+        self.assertEqual(html(data), expt)
+
+        data = ['tag#id.class', {'data': 'aaa'}]
+        expt = '<tag class="class" data="aaa" id="id" />'
+        self.assertEqual(html(data), expt)
+
+        data = ['tag#id.class', {'data': 'aaa', 'id': 'ID', 'class': 'CLASS'}]
+        expt = '<tag class="CLASS" data="aaa" id="ID" />'
+        self.assertEqual(html(data), expt)
+
+    def test_complex_tags(self):
+        data = [['a'], ['b']]
+        expt = '<a /><b />'
+        self.assertEqual(html(data), expt)
+
+        data = ['html', []]
+        expt = '<html></html>'
+        self.assertEqual(html(data), expt)
+
+        data = ['html', ['head'], ['body', ['p', 'hello world']]]
+        expt = '<html><head /><body><p>hello world</p></body></html>'
+        self.assertEqual(html(data), expt)
+
+        data = ['html', ['head'], ['body', {'data': 'foobar'}, ['p#contents', 'hello world'], ['br.line'], 'foo', []]]
+        expt = '<html><head /><body data="foobar"><p id="contents">hello world</p><br class="line" />foo</body></html>'
+        self.assertEqual(html(data), expt)
+
+if __name__ == "__main__":
+    unittest.main()
